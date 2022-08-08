c1cb32da1678f73236f4c0f80dff05e3a7bb39ec
Akshay Upadhya <akshay.upadhya@sonata-software.com>
2022-08-08 09:32:43 +0530

    Add project files.

---
+++ Data/SmallCartViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sonata_eStore.Models.ViewModels;
+using Sonata_eStore.Data;
+using Sonata_eStore.Models;
+
+namespace Sonata_eStore.Data
+{
+        public class SmallCartViewComponent : ViewComponent
+        {
+                public IViewComponentResult Invoke()
+                {
+                        List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+                        SmallCartViewModel smallCartVM;
+
+                        if (cart == null || cart.Count == 0)
+                        {
+                                smallCartVM = null;
+                        }
+                        else
+                        {
+                                smallCartVM = new()
+                                {
+                                        NumberOfItems = cart.Sum(x => x.Quantity),
+                                        TotalAmount = cart.Sum(x => x.Quantity * x.Price)
+                                };
+                        }
+
+                        return View(smallCartVM);
+                }
+        }
+}
---
+++ Models/ViewModels/SmallCartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Sonata_eStore.Models.ViewModels
+{
+        public class SmallCartViewModel
+        {
+                public int NumberOfItems { get; set; }
+                public decimal TotalAmount { get; set; }
+        }
+}
